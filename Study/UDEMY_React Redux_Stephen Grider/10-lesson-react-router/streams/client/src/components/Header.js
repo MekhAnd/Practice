ba09420d86c849c9c57224abedf53233c6c09438
Andrii.Mekhanich <andrii.mekhanich@gmail.com>
2022-10-05 15:18:44 +0300

    React-Redux ch21

--- Study/UDEMY_React Redux_Stephen Grider/10-lesson-react-router/streams/client/src/components/Header.js
+++ Study/UDEMY_React Redux_Stephen Grider/10-lesson-react-router/streams/client/src/components/Header.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
 
-const Header = ()=>{
-    return (
-        <div className="ui secondary pointing menu">
-            <Link to="/" className="item">
-                Streamer
-            </Link>
-            <div className="right menu">
-                <Link to="/" className="item">
-                    All Streams
-                </Link>
-            </div>
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamer
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth/>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
---
+++ Study/UDEMY_React Redux_Stephen Grider/10-lesson-react-router/streams/client/src/redusers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import authReduser from "./authReduser";
+
+export default combineReducers({
+  auth: authReduser,
+});
